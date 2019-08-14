cd0cf259054d2e1504ec53cb4593018b25cb8dac
commandantekaustav <kaustavbasu97@gmail.com>
2019-08-14 20:39:05 +0530

    AI Project added

---
+++ CPL/c_solutions/A2_CPL/cmake-build-debug/CMakeFiles/A2_CPL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A2_CPL.dir/main.c.obj"
+  "A2_CPL.pdb"
+  "A2_CPL.exe"
+  "A2_CPL.exe.manifest"
+  "libA2_CPL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A2_CPL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
